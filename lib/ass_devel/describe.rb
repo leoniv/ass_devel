94c0da633c867015b8b7fc1e2e288474c1d52853
Leonid Vlasov <leoniv.vlasov@gmail.com>
2016-11-21 09:41:49 +0700

    extract meta_data.rb describe.rb

---
+++ lib/ass_devel/describe.rb
@@ -0,0 +1,74 @@
+module AssDevel
+  module Describe
+    # @api private
+    module Mixins
+      # @api private
+      module MetaDataContainer
+        def md_object
+          @md_object || fail("Not initialized md container #{self.name}")
+        end
+
+        def is?(kalss)
+          md_object.instance_of? kalss
+        end
+
+        def meta_data(name)
+          @md_object = new_md_object(name)
+          @md_object.container = self
+          yield md_object if block_given?
+        end
+
+        def sources
+          fail 'FIXME'
+        end
+
+        def self.extended(base)
+          @app_src = base::APP_SRC
+        end
+
+        def app_src
+          @app_src
+        end
+      end
+    end
+
+    module DynamicDsl
+      DSL = {subsytem: MetaData::MdObjects::Subsystem,
+             common_module: FIXME
+      }
+
+    end
+
+    module Application
+      module Configuration
+        include Mixins::MetaDataContainer
+        def new_md_object(name)
+          MetaData::MdObjects::Configuration.new(name)
+        end
+      end
+
+      module Block
+        include Mixins::MetaDataContainer
+        def new_md_object(name)
+          MetaData::MdObjects::Subsystem.new(name, self::PREFIX)
+        end
+      end
+    end
+
+    module External
+      module DataProcessor
+        include Mixins::MetaDataContainer
+        def new_md_object(name)
+          MetaData::MdObjects::Externals::DataProcessor.new(name)
+        end
+      end
+
+      module Report
+        include Mixins::MetaDataContainer
+        def new_md_object(name)
+          MetaData::MdObjects::Externals::Report.new(name)
+        end
+      end
+    end
+  end
+end
