8695c39bccf36f74d920fae9471e1a83786fcce1
Leonid Vlasov <leoniv.vlasov@gmail.com>
2016-12-11 21:46:00 +0700

    fix dsl.rb

--- lib/ass_devel/dsl.rb
+++ lib/ass_devel/dsl.rb
@@ -3,7 +3,7 @@ module AssDevel
     module Application
       def application(name, &block)
         # FIXME: This is stub only
-        spec = ::Application::Specification.new
+        spec = AssDevel::Application::Specification.new
         spec.name = name
         spec.platform_require = self::PLATFORM_REQUIRE
         spec.src_root = self::SRC_ROOT
