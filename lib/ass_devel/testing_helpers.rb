4c3a37b76a0969239c564b3f7a0bbab17f319074
Leonid Vlasov <leoniv.vlasov@gmail.com>
2017-02-16 14:08:05 +0700

    add TestingHelpers::MetaData

--- lib/ass_devel/testing_helpers.rb
+++ lib/ass_devel/testing_helpers.rb
@@ -167,6 +167,472 @@ module AssDevel
       end
     end
 
+    # Provides dynamically generated mixins-helpers
+    # @example
+    #
+    # describe 'Tests for Catalogs.CatalogName' do
+    #   like_ole_runtime Runtimes::Ext
+    #   include AssDevel::TestingHelpers::MetaData::Catalogs[:CatalogName]
+    #
+    #   def catalog_item
+    #     new_item Description: 'Foo' do |obj|
+    #       obj.attr1 = 'bar'
+    #     end
+    #   end
+    #
+    #   it 'Item method #foo' do
+    #      catalog_item.foo.must_equal 'foo'
+    #   end
+    #
+    #   it 'ManagerModule method #bar' do
+    #     catalog_manager.bar.must_equal 'bar'
+    #   end
+    # end
+    module MetaData
+      module Managers
+        # @todo add DocumentManger, TaskManger etc.
+        module Abstract
+          module AbstractObjectManager
+            # Symbol like a :Catalogs, :Constants
+            def md_collection
+              fail 'Abstract method call'
+            end
 
+            def objects_manager
+              fail "Manager #{md_collection}.#{self.MD_NAME} not found" unless\
+                send(md_collection).ole_respond_to? self.MD_NAME
+              send(md_collection).send(self.MD_NAME)
+            end
+
+            def object_metadata
+              mEtadata.send(md_collection).send(self.MD_NAME)
+            end
+
+            # Abstract method must be redefined in module
+            # For example catatlogs makes with method :CreateItem
+            # but for Documents uses method :CreateDocument
+            def _new_object_method
+              fail 'Abstract method call'
+            end
+            private :_new_object_method
+
+            def _new_object
+              objects_manager.send(_new_object_method)
+            end
+            private :_new_object
+
+            # Make new object. Only make and not write
+            # @return [WIN32OLE] object not ref!
+            def new_object(**attributes, &block)
+              obj = _fill_attr(_new_object, **attributes)
+              yield obj if block_given?
+              obj
+            end
+
+            def _fill_attr(obj, **attributes)
+              attributes.each do |k, v|
+                obj.send("#{k}=", v)
+              end
+              obj
+            end
+            private :_fill_attr
+
+            # Make new object and write them
+            # @return [WIN32OLE] object not ref!
+            def make_object(**attributes, &block)
+              obj = new_object(**attributes, &block)
+              _write(obj)
+              obj
+            end
+
+            # If for write real object uses not :Write method it must be
+            # redefined in module
+            def _write(obj)
+              obj.Write()
+            end
+            private :_write
+          end
+
+          module AbstractGroupedObject
+
+            # Make new object grouo. Only make and not write
+            # @return [WIN32OLE] object not ref!
+            def new_group(**attributes, &block)
+              obj = _fill_attr(_new_group, **attributes)
+              yield obj if block_given?
+              obj
+            end
+
+            def _new_group
+              objects_manager.send(:CreateFolder)
+            end
+            private :_new_group
+
+            # Make new object group and write them
+            # @return [WIN32OLE] object not ref!
+            def make_group(**attributes, &block)
+              obj = new_group(**attributes, &block)
+              _write(obj)
+              obj
+            end
+          end
+
+          module AbstractRegisterManager
+            include AssOle::Snippets::Shared::Structure
+            include AssOle::Snippets::Shared::Array
+            require 'date'
+
+            def register_manager
+              fail "Manager #{md_collection}.#{self.MD_NAME} not found" unless\
+                send(md_collection).ole_respond_to? self.MD_NAME
+              send(md_collection).send(self.MD_NAME)
+            end
+
+            def register_record_key
+              ([:Period, :Recorder, :LineNumber] + register_dimensions).select do |k|
+                record_key.ole_respond_to? k
+              end
+            end
+
+            def register_metadata
+              mEtadata.send(md_collection).send(self.MD_NAME)
+            end
+
+            def register_dimensions
+              r = []
+              register_metadata.Dimensions.each do |d|
+                r << d.Name.to_sym
+              end
+              r
+            end
+
+            def record_set(**options, &block)
+              rs = register_manager.CreateRecordSet
+              options.each do |k, v|
+                rs.Filter.send(k).Set(v, true)
+              end
+              yield rs if block_given?
+              rs
+            end
+
+            def record_key(options = {})
+              register_manager.CreateRecordKey(structure(**options))
+            end
+          end
+
+          module CatalogManager
+            include AbstractObjectManager
+            include AbstractGroupedObject
+
+            def md_collection
+              :Catalogs
+            end
+
+            def _new_object_method
+              :CreateItem
+            end
+
+            alias_method :catalog_manager, :objects_manager
+            alias_method :catalog_metadata, :object_metadata
+            alias_method :new_item, :new_object
+            alias_method :make_item, :make_object
+            alias_method :new_folder, :new_group
+            alias_method :make_folder, :make_group
+          end
+
+          module ConstantManager
+            def md_collection
+              :Constants
+            end
+
+            def constant_manager
+              cOnstants.send(self.MD_NAME)
+            end
+
+            def constant_value_manager
+              constant_manager.CreateValueManager
+            end
+          end
+
+          module InformationRegisterManager
+            include AbstractRegisterManager
+
+            def md_collection
+              :InformationRegisters
+            end
+
+            def record_manager(**options, &block)
+              rm = register_manager.CreateRecordManager
+              options.each do |k, v|
+                rm.send("#{k}=", v)
+              end
+              yield rm if block_given?
+              rm
+            end
+          end
+        end
+
+        ABSTRACTS_ABSTRACTS = [:AbstractObjectManager,
+                               :AbstractRegisterManager,
+                               :AbstractGroupedObject]
+
+        ABSTRACTS = (Abstract.constants - ABSTRACTS_ABSTRACTS).map do |c|
+          Abstract.const_get(c)
+        end
+
+        # @api private
+        module ManagersHolder
+          def manager_holder?
+            true
+          end
+
+          def content
+            @content ||= {}
+          end
+
+          def [](md_name)
+            r = content_get(md_name.to_s)
+            fail ArgumentError,
+              "#{md_collection_name}[:#{md_name}] not found" unless r
+            r
+          end
+
+          def content_get(md_name)
+            return content[md_name] if content[md_name]
+
+            md_object = md_object_get(md_name)
+            content[md_object.Name] = build_manager(md_object, abstract_module)
+            content[md_name]
+          end
+
+          def md_object_get(md_name)
+            Class.new do
+              def initialize(md_name)
+                @md_name = md_name
+              end
+
+              def Name
+                @md_name
+              end
+            end.new(md_name)
+          end
+
+          def build_manager(md_object, abstract_module)
+            r = Module.new do
+              include abstract_module
+
+              define_method :MD_NAME do
+                md_object.Name
+              end
+            end
+          end
+
+          def method_missing(method, *args)
+            content_get(method)
+          end
+
+          attr_reader :abstract_module
+          attr_reader :md_collection_name
+        end
+
+        def self.build_manager_holder(md_collection_name, abstract_module)
+          Module.new do
+            extend ManagersHolder
+            @abstract_module = abstract_module
+            @md_collection_name = md_collection_name
+          end
+        end
+
+        def self.init
+          ABSTRACTS.each do |abstract_module|
+            const_name = Module.new {extend abstract_module}.md_collection
+            const_set const_name,
+              build_manager_holder(const_name, abstract_module)
+          end
+        end
+
+        def self.method_missing(method, **args)
+          manager_holder = const_get method
+          fail NoMethodError,
+            "undefined method `#{method}' for #{self.name}" unless\
+            manager_holder.respond_to? :manager_holder?
+          manager_holder
+        end
+
+        init
+      end
+
+      # @todo rename AutoFixtures to ?
+      # @example
+      #  include MetaData::AutoFixtures
+      #
+      #  ref = auto_fixtures.Catalogs.CtalogName.ref Attr1: 'Value' do |item|
+      #    item.Attr2 = 'Value2'
+      #    item[:TabularSection].add Attr1: 'Value' do |tab_item|
+      #      tab_item.Attr2 = 'Value2
+      #    end
+      #  end
+      module AutoFixtures
+        module ObjectBuilder
+          class ObjectWrapper
+            class TabularSection
+              attr_reader :ole_ts
+              def initialize(ole_ts)
+                @ole_ts = ole_ts
+              end
+
+              def add(**values, &block)
+                item = ole_ts.Add
+                values.each do |k, v|
+                  item.send("#{k}=", v)
+                end
+                yield item if block_given?
+                item
+              end
+            end
+
+            def tabular(name)
+              TabularSection.new(ole_obj.send(name))
+            end
+            alias_method :[], :tabular
+
+            attr_reader :ole_obj
+            def initialize(ole_obj)
+              @ole_obj = ole_obj
+            end
+
+            def method_missing(method, *args)
+              ole_obj.send(method, *args)
+            end
+          end
+
+          def wrapp_(ole)
+            ObjectBuilder::ObjectWrapper.new(ole)
+          end
+          private :wrapp_
+
+          def new(**attributes, &block)
+            ow = wrapp_ new_object(**attributes)
+            yield ow if block_given?
+            ow.ole_obj
+          end
+
+          def make(**attributes, &block)
+            ow = wrapp_ new(**attributes, &block)
+            _write(ow.ole_obj)
+            ow.ole_obj
+          end
+
+          def ref(**attributes, &block)
+            make(**attributes, &block).Ref
+          end
+
+          # @todo implements refs finder
+          def find(**attributes, &block)
+            fail NotImplementedError
+          end
+        end
+
+        module GroupedObjectBuilder
+          include ObjectBuilder
+
+          def g_new(**attributes, &block)
+            ow = wrapp_ new_group **attributes
+            yield ow if block_given?
+            ow.ole_obj
+          end
+
+          def g_make(**attributes, &block)
+            ow = wrapp_ g_new(**attributes, &block)
+            _write ow.ole_obj
+            ow.ole_obj
+          end
+
+          def g_ref(**attributes, &block)
+            g_make(**attributes, &block).Ref
+          end
+        end
+
+        module RegisterBuilder
+          class Records
+            attr_reader :ole_rs
+            def initialize(ole_rs)
+              @ole_rs = ole_rs
+            end
+
+            def add(**values, &block)
+              rec = ole_rs.Add
+              values.each do |k, v|
+                rec.send("#{k}=", v)
+              end
+              yield rec if block_given?
+              rec
+            end
+          end
+
+          def records(**filter, &block)
+            rs = record_set(**filter)
+            yield Records.new(rs)
+            rs.write false
+            rs
+          end
+        end
+
+        class FixtureFactory
+          VALID_RUNTIMES = [:external, :thick]
+
+          attr_reader :ole_runtime
+          def initialize(ole_runtime)
+            @ole_runtime = ole_runtime
+            validate_ole_runtime_type?
+          end
+
+          def validate_ole_runtime_type?
+            fail "Runtime must be :external or :thick" unless\
+              VALID_RUNTIMES.include? self.ole_runtime.ole_type
+          end
+
+          def method_missing(method, *args)
+            calls_stack << method
+            return execute_stack if calls_stack.size == 2
+            self
+          end
+
+          def execute_stack
+            manager_name = calls_stack.shift
+            md_name = calls_stack.shift
+            manager = MetaData::Managers.send(manager_name).send(md_name)
+            new_builder(mixin_get(manager_name), manager, ole_runtime)
+          end
+
+          def mixin_get(manager_name)
+            case manager_name
+            when :Catalogs then GroupedObjectBuilder
+              # TODO: when ...
+            when :InformationRegisters then RegisterBuilder
+              # TODO: when ...
+            else
+              fail "Invalid manager_name `#{manager_name}'"
+            end
+          end
+
+          def new_builder(mixin, manager, runtime)
+            Class.new do
+              like_ole_runtime runtime
+              include manager
+              include mixin
+            end.new
+          end
+
+          def calls_stack
+            @calls_stack ||= []
+          end
+        end
+
+        def auto_fixtures
+          MetaData::AutoFixtures::FixtureFactory.new ole_runtime_get
+        end
+      end
+    end
   end
 end
---
+++ test/ass_devel/testing_helpers/meta_data_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+require 'ass_devel/testing_helpers'
+
+module AssDevelTest
+  module TestingHelpers
+    describe AssDevel::TestingHelpers::MetaData do
+      it 'FIXME' do
+        skip
+      end
+    end
+  end
+end
