9e16c9f64eab55888e8e64bf3ca1213f1a642c2e
Leonid Vlasov <leoniv.vlasov@gmail.com>
2017-04-29 12:57:00 +0700

    Form wrapper add #get_action #exec_action

--- lib/ass_devel/testing_helpers.rb
+++ lib/ass_devel/testing_helpers.rb
@@ -935,6 +935,16 @@ module AssDevel
             end
             r
           end
+
+          def get_action(action)
+            server.call("GetAction", action.to_s)
+          end
+
+          def exec_action(action, *args)
+            method = get_action(action)
+            fail ArgumentError, "Invalid action `#{action}'" if method.empty?
+            client.send(method, *args)
+          end
         end
       end
     end
